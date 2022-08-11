d785954a6f37dd701c9f47a3cbc52d2e99472142
cijovigor <cijovigor@yahoo.com>
2022-08-11 17:47:38 +0200

    update 11.08.2022

--- src/homework14/MyArrayDataException.java
+++ src/homework14/MyArrayDataException.java
@@ -1,6 +1,6 @@
 package homework14;
 
-public class MyArrayDataException extends RuntimeException {
+public class MyArrayDataException extends Exception {
     public MyArrayDataException(int row, int column){
         super("Incorrect number format in row: " + row + ", column: "+column);
     }
